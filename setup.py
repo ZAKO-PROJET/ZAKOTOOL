a726cb975785bab6e95f43f0fee324331a5e247f
ZAKOTOOLS <108414850+ZAKOTOOLS@users.noreply.github.com>
2022-06-29 16:46:24 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+
+os.system("sudo apt install nmap")
+os.system("sudo apt-get -y install gobuster")
+os.system("sudo apt install ruby")
+os.system("sudo apt install build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev  libgmp-dev zlib1g-dev")
+os.system("sudo gem install wpscan")
+os.system("sudo apt-get install hydra-gtk")
+os.system("sudo mv common.txt /usr/share")
+os.system("sudo mv bigone.txt /usr/share")
+os.system("sudo apt install john")
+os.system("sudo mv zako /bin/")
+os.system("sudo wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt")
+os.system("sudo mv rockyou.txt /usr/share")
